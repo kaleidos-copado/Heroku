394ee41672b055137fb5c95ffe7fe9c679cfb762
kaleidos-copado <77468744+kaleidos-copado@users.noreply.github.com>
2021-01-14 20:51:35 -0800

    make it better

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  port: process.env.PORT || 3000,
+  db: process.env.MONGODB || 'mongodb+srv://jorgecopado17:admin123@cluster0.xdwf9.mongodb.net/Pr2?retryWrites=true&w=majority',
+  
+  urlParser: {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+
+};
